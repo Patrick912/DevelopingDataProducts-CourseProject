689de5fa81048ec721a0188e6d0d81c3a41a19c4
Patrick912 <pneumann912@gmail.com>
2020-09-04 16:16:51 +0400

    Finished shiny app

---
+++ Shiny/CourseApp/server.R
@@ -0,0 +1,39 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(caret)
+library(ggplot2)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+
+    # defining training control 
+    # as cross-validation and  
+    # value of K equal to 5 
+    train_control <- trainControl(method = "cv", 
+                                  number = 5) 
+    set.seed(383743)
+    model <- train(waiting ~., data = faithful, method='rf', trControl = train_control)
+    model
+    
+    output$wait_time <- renderText({
+        predict(model, data.frame(eruptions = c(input$eruption_length)))
+    })
+    
+    output$distPlot <- renderPlot({
+        y_val <- predict(model, data.frame(eruptions = c(input$eruption_length)))
+        # generate bins based on input$bins from ui.R
+        g <- ggplot(data=faithful, aes(eruptions, waiting)) + 
+            geom_point() + 
+            geom_point(aes(x=input$eruption_length, y=y_val), colour="red", fill="red", shape = 23, size = 4)
+        g
+    })
+
+})
---
+++ Shiny/CourseApp/ui.R
@@ -0,0 +1,36 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Predictor"),
+
+    # Sidebar with input for last eruption time
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("eruption_length",
+                        "Lenght of last eruption:",
+                        min = 0.4,
+                        max = 7,
+                        step = 0.2,
+                        value = 3)
+        ),
+
+        # Show a plot of faithful data plus the predicted waiting time based
+        # on user input
+        mainPanel(
+            h4('Predicted waiting time for next eruption in seconds:'),
+            textOutput("wait_time"),
+            plotOutput("distPlot")
+        )
+    )
+))
